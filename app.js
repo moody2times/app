1995c6d65e3789503c18abfc5ae445f770c139f1
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-10-17 20:29:16 +0100

    :construction: Define the server boilerplate

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import crypto from "crypto";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	const demoKey = crypto.randomBytes(32).toString("hex");
+	res.status(200).send(demoKey);
+});
+
+app.listen(3000, () => {
+	console.log("running on port 3000");
+});
