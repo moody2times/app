35df582cc6cdf1b18436a9a1fbde86ad3511cc7e
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-10-17 20:36:50 +0100

    :construction: Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
 	const demoKey = crypto.randomBytes(32).toString("hex");
-	res.status(200).send(demoKey);
+	res.status(200).json(demoKey);
 });
 
 app.listen(3000, () => {
