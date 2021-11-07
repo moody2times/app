c4a0ad59b449cf1b2fc48cc4385f479ad26f05f6
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-11-07 14:57:09 +0100

    :recycle: Refactor app.css import, remove index.css import

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { addLike, addDislike } from "./features/likes/likeSlice";
-import "./App.css";
+import "./styles/App.css";
 
 function App() {
 	const like = useSelector((state) => state.like.like);
