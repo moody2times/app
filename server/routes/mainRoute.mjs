99fe31fd3820ca466bd3ed4b106d2983bd6b44c4
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-11-09 19:07:31 +0100

    :construction: Update mainRoute

--- server/routes/mainRoute.mjs
+++ server/routes/mainRoute.mjs
@@ -3,10 +3,6 @@ import express from "express";
 const router = express.Router();
 
 //home page route
-router.get("/", (req, res) => {
-	res.end("home page");
-});
-
 router.get("/login", (req, res) => {
 	res.end("login page");
 });
