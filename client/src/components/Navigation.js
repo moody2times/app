711477cc87963e1561eaba73b62a549002d103a9
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-11-11 17:48:23 +0100

    :construction: Update navigation component

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,4 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export const Navigation = () => {};
+export const Navigation = () => {
+	return (
+		<div>
+			<h1>meet N greet</h1>
+		</div>
+	);
+};
