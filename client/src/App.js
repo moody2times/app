5c4380a44722ae000f4a8204bab96635b4c24225
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-11-11 17:49:40 +0100

    :construction: Update App

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,15 @@
 import React from "react";
+import { Navigation } from "./components/Navigation";
 import { useSelector, useDispatch } from "react-redux";
 import { addLike, addDislike } from "./features/likes/likeSlice";
 import "./styles/App.css";
 
 function App() {
-	const like = useSelector((state) => state.like.like);
-	const dislike = useSelector((state) => state.like.dislike);
-	const dispatch = useDispatch();
+	// const like = useSelector((state) => state.like.like);
+	// const dislike = useSelector((state) => state.like.dislike);
+	// const dispatch = useDispatch();
 
-	const handleLikeClick = () => {
-		dispatch(addLike());
-	};
-
-	const handleDislikeClick = () => {
-		dispatch(addDislike());
-	};
-
-	return (
-		<React.Fragment>
-			<h1>meetNgreet</h1>
-			<p>{like}</p>
-			<p>{dislike}</p>
-			<button onClick={handleLikeClick}>Like</button>
-			<button onClick={handleDislikeClick}>Dislike</button>
-		</React.Fragment>
-	);
+	return <Navigation />;
 }
 
 export default App;
