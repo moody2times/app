7610f7573f99c4474ee8dd36d112abe5b0e88628
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-11-12 20:15:07 +0100

    :lipstick: Add universal style

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import store from "./store/store";
 import { Provider } from "react-redux";
 import App from "./App";
+import styles from "./styles/Index.module.css";
 
 ReactDOM.render(
 	<Provider store={store}>
