47521b71ad0759e931d00870bc5b30a8a8469385
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-11-12 19:52:10 +0100

    :construction: Update navigation

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -5,7 +5,7 @@ import styles from "../styles/Navigation.module.css";
 export const Navigation = () => {
 	return (
 		<div className={styles.navigation}>
-			<h1 className={styles.heading}>meet N greet</h1>
+			<h1 className={styles.heading}>meet-N-greet</h1>
 		</div>
 	);
 };
