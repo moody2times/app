04c254f7fb91cee0eab1343d3ad233be84ed637a
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-11-13 19:41:15 +0100

    :construction: Set up Router

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import store from "./store/store";
 import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import styles from "./styles/Index.module.css";
 
 ReactDOM.render(
 	<Provider store={store}>
-		<App />
+		<Router>
+			<App />
+		</Router>
 	</Provider>,
 	document.getElementById("root")
 );
