1f3e2aac0bcfb63b8bcdd7ed95fda9784fab5ed1
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-11-13 19:28:43 +0100

    :construction: Define home link

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,11 +1,16 @@
 import React from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styles from "../styles/Navigation.module.css";
 
 export const Navigation = () => {
 	return (
-		<div className={styles.navigation}>
-			<h1 className={styles.heading}>meet-N-greet</h1>
-		</div>
+		<React.Fragment>
+			<div className={styles.navigation}>
+				<h1 className={styles.heading}>meet-N-greet</h1>
+			</div>
+			<div>
+				<Link>Home</Link>
+			</div>
+		</React.Fragment>
 	);
 };
