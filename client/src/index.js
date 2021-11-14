c2a3e4e4c4576273d53a05c0e5a7705b41b8aa74
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-11-14 19:42:14 +0100

    :recycle: Refactor index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import store from "./store/store";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-import styles from "./styles/Index.module.css";
+import "./styles/index.css";
 
 ReactDOM.render(
 	<Provider store={store}>
