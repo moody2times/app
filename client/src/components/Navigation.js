48326c19dab5bdad9e52de3cfed3c99f800a6ab2
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-11-14 19:47:24 +0100

    :recycle: Refactor navigation.js

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -5,14 +5,14 @@ import styles from "../styles/Navigation.module.css";
 export const Navigation = () => {
 	return (
 		<React.Fragment>
-			<div className={styles.navigation}>
-				<h1 className={styles.heading}>meet-N-greet</h1>
-			</div>
 			<div>
-				<Link to="/">Home</Link>
-				<Link to="/profile">Profile</Link>
-				<Link to="/friends">Friends</Link>
-				<Link to="/settings">Settings</Link>
+				<h1>meet-N-greet</h1>
+				<div>
+					<Link to="/">Home</Link>
+					<Link to="/profile">Profile</Link>
+					<Link to="/friends">Friends</Link>
+					<Link to="/settings">Settings</Link>
+				</div>
 			</div>
 		</React.Fragment>
 	);
