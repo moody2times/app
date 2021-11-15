c09455828959ffe356470f325ac27b81d730f906
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-11-15 19:28:03 +0100

    :construction: Add registerHandler

---
+++ server/controller/register.mjs
@@ -0,0 +1,12 @@
+export const registerHandler = (req, res) => {
+	const { username, password } = req.body;
+
+	//validate username and password
+
+	//hash password
+
+	//store user in database
+
+	res.status(200);
+	res.end();
+};
