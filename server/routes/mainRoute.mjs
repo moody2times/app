89efbb38c79acd46d91e44131414b25415c22c15
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-11-15 19:30:23 +0100

    :construction: Consume loginHandler

--- server/routes/mainRoute.mjs
+++ server/routes/mainRoute.mjs
@@ -1,11 +1,10 @@
 import express from "express";
+import { loginHandler } from "../controller/login.mjs";
 
 const router = express.Router();
 
 //home page route
-router.post("/login", (req, res) => {
-	res.end("login page");
-});
+router.post("/login", loginHandler);
 
 router.post("/register", (req, res) => {
 	res.end("register page");
