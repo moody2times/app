29153af08ed770c34d9a5fa3c6da3c272d8b4b77
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-11-17 18:51:26 +0100

    :construction: Consume users

--- server/routes/userRoute.mjs
+++ server/routes/userRoute.mjs
@@ -2,8 +2,6 @@ import { Router } from "express";
 import { users } from "../controller/users.mjs";
 const userRouter = Router();
 
-userRouter.get("/:username", (req, res) => {
-	res.end("user page");
-});
+userRouter.get("/:username", users);
 
 export default userRouter;
