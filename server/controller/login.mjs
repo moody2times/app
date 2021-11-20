6e09083708f066dec9d6b0bed4a8904dafb133e9
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-11-20 19:27:21 +0100

    :construction: Update loginHandler

--- server/controller/login.mjs
+++ server/controller/login.mjs
@@ -1,10 +1,11 @@
 export const loginHandler = (req, res) => {
 	//validate username and password
+	const { username, password } = req.body;
 
 	//check user against database
 
 	//authenticate user
 
 	res.status(200);
-	res.end("<h1>Welcome</h1>");
+	res.end(`${username} and ${password}`);
 };
