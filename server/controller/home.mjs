0f512130cf2eb105ea8864155901bdd1d36a9994
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-11-21 19:58:18 +0100

    :construction: Update home

--- server/controller/home.mjs
+++ server/controller/home.mjs
@@ -1,7 +1,10 @@
+import { config } from "dotenv";
+config();
+
 const KEY = process.env.KEY;
 
 export const homeHandler = (req, res) => {
 	//home route handler
 	res.cookie("userId", KEY, { maxAge: 60 * 60 * 24, httpOnly: true });
-	res.end(`Welcome home ${KEY}`);
+	res.end(`Welcome home`);
 };
