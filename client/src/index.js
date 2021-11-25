aa45951726021d384dec4c91ab287140fc66868f
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-11-25 19:27:32 +0100

    :construction: Update index css import

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import store from "./store/store";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-import "./styles/index.css";
+import "./styles/Index.css";
 
 ReactDOM.render(
 	<Provider store={store}>
