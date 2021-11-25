819115d614d1c32dbffa2949786849ec8b8579f2
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-11-25 19:38:36 +0100

    :recycle: Refactor navigation

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -5,13 +5,11 @@ import navStyle from "../styles/Navigation.module.css";
 export const Navigation = () => {
 	return (
 		<div className={navStyle.banner}>
-			<div>
-				<h1>meet-N-greet</h1>
-				<Link to="/">Home</Link>
-				<Link to="/profile">Profile</Link>
-				<Link to="/friends">Friends</Link>
-				<Link to="/settings">Settings</Link>
-			</div>
+			<h1>meet-N-greet</h1>
+			<Link to="/">Home</Link>
+			<Link to="/profile">Profile</Link>
+			<Link to="/friends">Friends</Link>
+			<Link to="/settings">Settings</Link>
 		</div>
 	);
 };
