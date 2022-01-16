36e6b35ec772c26daebf1434e834a134c9a0f02e
Uwojeya Moody Mudiaga <moody.mudiaga@gmail.com>
2022-01-16 21:19:03 +0100

    :new: Add Home.js component

---
+++ client/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Home() {
+	return (
+		<React.Fragment>
+			<div>
+				<h1 className="display">Nakama</h1>
+				<p className="text">
+					Take your close friends with you wherever you go. Go beyond your
+					limits because your friends are near.
+				</p>
+			</div>
+		</React.Fragment>
+	);
+}
+
+export default Home;
