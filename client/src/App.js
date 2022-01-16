e44b902986cac9c8046013053bc00fe4f8d51bd7
Uwojeya Moody Mudiaga <moody.mudiaga@gmail.com>
2022-01-16 21:41:01 +0100

    :recycle: Update App.js import

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
-import { Navigation } from "./components/Navigation";
+import Navigation from "./components/Navigation";
 import Home from "./components/Home";
 // import { useSelector, useDispatch } from "react-redux";
 // import { addLike, addDislike } from "./features/likes/likeSlice";
